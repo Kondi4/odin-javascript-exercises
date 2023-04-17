88c1d030a0387982887d897c4c5c54b74ebbf0f1
Kondi4 <58360159+Kondi4@users.noreply.github.com>
2023-04-17 20:25:39 +0200

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
 
+    for (let i=0; i<array.length; i++){
+        let j=0;
+        while (array[i] !== args[j] && j < args.length){
+            j++;
+        }
+        if (j == args.length){
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
