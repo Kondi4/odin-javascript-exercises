14ea6b7e828c1bb25ce412000e3dfe751046810e
Kondi4 <58360159+Kondi4@users.noreply.github.com>
2023-04-19 20:04:14 +0200

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(leftNumber, rightNumber) {
+    let result = 0;
 
+    if (leftNumber < 0 || rightNumber < 0 || typeof(leftNumber) != "number" || typeof(rightNumber) != "number") {
+        return "ERROR";
+    }
+    else if (leftNumber > rightNumber) {
+        leftNumber = leftNumber ^ rightNumber;
+        rightNumber = leftNumber ^ rightNumber;
+        leftNumber = leftNumber ^ rightNumber;
+    }
+
+    for (let i=leftNumber; i<=rightNumber; i++) {
+        result+=i;
+    }
+
+    return result;
 };
 
-// Do not edit below this line
+// Do not edit rightNumberelow this line
 module.exports = sumAll;
